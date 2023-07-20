6e9bb60f71810321c453ace9acbd5cfbcd7b537c
Oliver <oliverrodriguez777@hotmail.com>
2023-07-20 16:25:25 -0500

    Adding worckflow for testing api and adding utilities

---
+++ utilities/setter.py
@@ -0,0 +1,18 @@
+# Vamos a realizar un path para google apication credentials
+
+# para leer varibles de ambiente
+import os 
+
+# el json dl secret esta codificado con base 64 y hay que decodearlo
+from base64 import b64decode
+
+def main():
+    key = os.environ.get('SERVICE_ACCOUNT_KEY')
+    with open('path.json', 'w') as json_file:
+        # aqui se decode
+        json_file.write(b64decode(key.decode()))
+        # en testing.yaml está esperando una salida por esto hacemos:
+    print(os.path.realpath('path.json'))
+    
+if __name__ == '__main__':
+    main()
